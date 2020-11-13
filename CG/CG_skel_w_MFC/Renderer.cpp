7cedaa7f8684ca1dbd452577e2e77c089b3bb233
Etiel Sofer <50843359+etlsofer@users.noreply.github.com>
2020-11-13 14:06:48 +0200

    Add files via upload

--- CG/CG_skel_w_MFC/Renderer.cpp
+++ CG/CG_skel_w_MFC/Renderer.cpp
@@ -6,6 +6,42 @@
 
 #define INDEX(width,x,y,c) (x+y*width)*3+c
 
+GLfloat get_max_of_x(const vector<vec3>* vertices) {
+	GLfloat max = 0;
+	for (int i = 0; i < vertices->size() - 1; i++) {
+		if ((*vertices)[i].x >= max) max = (*vertices)[i].x;
+	}
+	return max;
+}
+
+GLfloat get_max_of_y(const vector<vec3>* vertices) {
+	GLfloat max = 0;
+	for (int i = 0; i < vertices->size() - 1; i++) {
+		if ((*vertices)[i].y >= max) max = (*vertices)[i].y;
+	}
+	return max;
+}
+
+GLfloat get_min_of_x(const vector<vec3>* vertices) {
+	GLfloat min = 10000;
+	for (int i = 0; i < vertices->size() - 1; i++) {
+		if ((*vertices)[i].x <= min) min = (*vertices)[i].x;
+	}
+	return min;
+}
+
+GLfloat get_min_of_y(const vector<vec3>* vertices) {
+	GLfloat min = 10000;
+	for (int i = 0; i < vertices->size() - 1; i++) {
+		if ((*vertices)[i].y <= min) min = (*vertices)[i].y;
+	}
+	return min;
+}
+
+int normal(GLfloat input, GLfloat output_start, GLfloat output_end, GLfloat input_start, GLfloat input_end) {
+	return int(output_start + ((output_end - output_start) * (input - input_start)) / (input_end - input_start));
+}
+
 Renderer::Renderer() :m_width(512), m_height(512)
 {
 	InitOpenGLRendering();
@@ -47,12 +83,51 @@ void Renderer::SetDemoBuffer()
 	}
 }
 
-void Renderer::DrawTriangles(const vector<vec3>* vertices, const vector<vec3>* normals) {
-	//horizontal line
-	for (int i = 0; i < m_width; i++)
-	{
-		m_outBuffer[INDEX(m_width, i, 256, 0)] = 1;	m_outBuffer[INDEX(m_width, i, 256, 1)] = 0;	m_outBuffer[INDEX(m_width, i, 256, 2)] = 1;
 
+
+
+
+void Renderer::DrawTriangles(const vector<vec3>* vertices, const vector<vec3>* normals) {
+	//clean bufer
+	//for (int i = 0; i < m_width; i++)
+	//	for (int j = 0; j < m_height; j++) {
+	//		m_outBuffer[INDEX(m_width, i, j, 0)] = 0;
+	//	}
+	
+	GLfloat max_x = get_max_of_x(vertices);
+	GLfloat max_y = get_max_of_y(vertices);
+	GLfloat min_x = get_min_of_x(vertices);
+	GLfloat min_y = get_min_of_y(vertices);
+	for (int i = 0; i < vertices->size()-1; i++)
+	{
+		int x1 = normal((*vertices)[i].x, 0, m_width-1, min_x, max_x);
+		int x2 = normal((*vertices)[i+1].x, 0, m_width-1, min_x, max_x);
+		int y1 = normal((*vertices)[i].y, 0, m_height-1, min_y, max_y);
+		int y2 = normal((*vertices)[i + 1].y, 0, m_height-1, min_y, max_y);
+		//int m = int((y2-y1)/(x2-x1));
+		//printf("%d %d %d %d\n",x1,x2,y1,m);
+		//printf("%d\n", m);
+		m_outBuffer[INDEX(m_width, x1, y1, 1)] = 1;
+		m_outBuffer[INDEX(m_width, x2, y2, 1)] = 1;
+		
+		//int m_new = 2 * (y2 - y1);
+		//int slope_error_new = m_new - (x2 - x1);
+		//for (int x = x1, y = y1; x <= x2; x++)
+		//{
+		//	m_outBuffer[INDEX(m_width, x, y, 0)] = 1;
+
+			// Add slope to increment angle formed 
+			//slope_error_new += m_new;
+
+			// Slope error reached limit, time to 
+			// increment y and update slope error. 
+			//if (slope_error_new >= 0)
+			//{
+			//	y++;
+			//	slope_error_new -= 2 * (x2 - x1);
+			//}
+		//}
+		
 	}
 }
 
