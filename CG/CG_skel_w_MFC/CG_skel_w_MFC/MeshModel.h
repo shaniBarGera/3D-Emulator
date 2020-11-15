935dc4fbb2776ce1d8826a2dc8c659e9c3387037
shaniBarGera <shani.bla@gmail.com>
2020-11-15 17:55:24 +0200

    line working

---
+++ CG/CG_skel_w_MFC/CG_skel_w_MFC/MeshModel.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "scene.h"
+#include "vec.h"
+#include "mat.h"
+#include <string>
+
+using namespace std;
+
+class MeshModel : public Model
+{
+protected :
+	MeshModel() {}
+	
+	//add more attributes
+	
+	bool normal = false;
+public:
+	mat4 _world_transform;
+	mat3 _normal_transform;
+
+	mat4 transform;
+	mat4 projection;
+
+	MeshModel(string fileName);
+	~MeshModel(void);
+	void loadFile(string fileName);
+	void draw();
+	vector<vec3> vertex_positions;
+	vector<vec3> vertex_normal;
+	
+};
