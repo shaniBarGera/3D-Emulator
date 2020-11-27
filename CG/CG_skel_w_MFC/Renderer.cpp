e053f2c673248e9ba761cd7ee20ab12d2ecda0ba
shaniBarGera <shani.bla@gmail.com>
2020-11-27 11:29:49 +0200

    Add files via upload

--- CG/CG_skel_w_MFC/Renderer.cpp
+++ CG/CG_skel_w_MFC/Renderer.cpp
@@ -10,11 +10,13 @@ Renderer::Renderer() :m_width(512), m_height(512)
 {
 	InitOpenGLRendering();
 	CreateBuffers(512,512);
+	bbox = false;
 }
 Renderer::Renderer(int width, int height) :m_width(width), m_height(height)
 {
 	InitOpenGLRendering();
 	CreateBuffers(width,height);
+	bbox = false;
 }
 
 Renderer::~Renderer(void)
@@ -36,20 +38,222 @@ void Renderer::SetDemoBuffer()
 	//vertical line
 	for(int i=0; i<m_width; i++)
 	{
-		m_outBuffer[INDEX(m_width,256,i,0)]=1;	m_outBuffer[INDEX(m_width,256,i,1)]=0;	m_outBuffer[INDEX(m_width,256,i,2)]=0;
+		m_outBuffer[INDEX(m_width,256,i,0)]=1;
+		m_outBuffer[INDEX(m_width,256,i,1)]=0;	
+		m_outBuffer[INDEX(m_width,256,i,2)]=0;
 
 	}
 	//horizontal line
 	for(int i=0; i<m_width; i++)
 	{
-		m_outBuffer[INDEX(m_width,i,256,0)]=1;	m_outBuffer[INDEX(m_width,i,256,1)]=0;	m_outBuffer[INDEX(m_width,i,256,2)]=1;
+		m_outBuffer[INDEX(m_width,i,256,0)]=1;
+		m_outBuffer[INDEX(m_width,i,256,1)]=0;
+		m_outBuffer[INDEX(m_width,i,256,2)]=1;
 
 	}
 }
 
+void Renderer::setPixelOn(int x, int y, char color) {
+	if (x < 0 || x >= m_width || y < 0 || y >= m_height) return;
+	
+	switch (color) {
+	case 'r' :
+		m_outBuffer[INDEX(m_width - 1, x, y, 0)] = 1;
+		break;
+	case 'g':
+		m_outBuffer[INDEX(m_width - 1, x, y, 1)] = 1;
+		break;
+	case 'b':
+		m_outBuffer[INDEX(m_width - 1, x, y, 2)] = 1;
+		break;
+	case 'p':
+		m_outBuffer[INDEX(m_width - 1, x, y, 0)] = 1;
+		m_outBuffer[INDEX(m_width - 1, x, y, 2)] = 1;
+		break;
+	case 't':
+		m_outBuffer[INDEX(m_width - 1, x, y, 1)] = 1;
+		m_outBuffer[INDEX(m_width - 1, x, y, 2)] = 1;
+		break;
+	case 'w':
+		m_outBuffer[INDEX(m_width - 1, x, y, 0)] = 1;
+		m_outBuffer[INDEX(m_width - 1, x, y, 1)] = 1;
+		m_outBuffer[INDEX(m_width - 1, x, y, 2)] = 1;
+		break;
+	}
+}
+
+int Sign(int dxy)
+{
+	if (dxy < 0) return -1;
+	else if (dxy > 0) return 1;
+	else return 0;
+}
+
+void Renderer::Drawline(int x1, int x2, int y1, int y2, char color) {
+	//printf("draw line %d %d %d %d\n", x1, x2, y1, y2);
+	int Dx = x2 - x1;
+	int Dy = y2 - y1;
+
+	//# Increments
+	int Sx = Sign(Dx);
+	int Sy = Sign(Dy);
+
+	//# Segment length
+	Dx = abs(Dx);
+	Dy = abs(Dy);
+	int D = max(Dx, Dy);
+
+	//# Initial remainder
+	double R = D / 2;
+
+	int X = x1;
+	int Y = y1;
+	if (Dx > Dy)
+	{
+		//# Main loop
+		for (int I = 0; I < D; I++)
+		{
+			setPixelOn(X, Y, color);
+			//# Update (X, Y) and R
+			X += Sx; R += Dy; //# Lateral move
+			if (R >= Dx)
+			{
+				Y += Sy;
+				R -= Dx; //# Diagonal move
+			}
+		}
+	}
+	else
+	{
+		//# Main loop
+		for (int I = 0; I < D; I++)
+		{
+			setPixelOn(X, Y, color);
+			//# Update (X, Y) and R
+			Y += Sy;
+			R += Dx; //# Lateral move
+			if (R >= Dy)
+			{
+				X += Sx;
+				R -= Dy; //# Diagonal move
+			}
+		}
+	}
+}
 
 
+void Renderer::DrawTriangles(const vector<vec3>* eye, const vector<vec3>* vertices, const vector<vec3>* normals) {
+	//printf("DRAW TRIANGLE\n");
 
+	for (int j = 0; j < eye->size(); j++) {
+		vec4 temp = vec4((*eye)[j]);
+		temp = STransform * Projection * CTransform * temp;
+		GLfloat x = temp.x / temp.w;
+		GLfloat y = temp.y /temp.w;
+		Drawline(x - 3, x + 3, y, y, 'w');
+		Drawline(x, x, y - 3, y + 3, 'w');
+	}
+
+	int length = (bbox) ? vertices->size() - 36 : vertices->size();
+	for (int i = 0; i < length; i+=3)
+	{
+		GLfloat x[3] = { 0 };
+		GLfloat y[3] = { 0 };
+		vec4 center(0);
+
+		vec4 f_normal;
+		
+		for (int j = 0; j < 3; j++) {
+			center += vec4((*vertices)[i + j]);
+			vec4 temp = WTransform * MTransform * vec4((*vertices)[i + j]);
+			vec4 normal = NTransform * vec4((*normals)[i + j]) * 0.2;
+			normal.w = 0;
+			
+			if (j == 0) {
+				f_normal = normal;
+			}
+
+			
+			normal += temp;
+			temp = STransform * Projection * CTransform * temp;
+
+			normal = STransform * Projection * CTransform * normal;
+			vec3 n = vec4t3(normal);
+
+			vec3 v = vec4t3(temp);
+			x[j] = v.x;
+			y[j] = v.y;
+
+
+			if (show_normalsV) {
+				Drawline(x[j], n.x, y[j], n.y, 'g');
+			}
+		}
+		
+		char color = 'p';
+
+		Drawline(x[0], x[1], y[0], y[1], color);
+		Drawline(x[2], x[1], y[2], y[1], color);
+		Drawline(x[0], x[2], y[0], y[2], color);
+	
+		vec4 v1 = (*vertices)[i];
+		vec4 v2 = (*vertices)[i + 1];
+		vec4 v3 = (*vertices)[i + 2];
+		center = (v1 +v2 +v3) / 3;
+		vec4 temp_center = WTransform * MTransform * center;
+		f_normal += temp_center;
+
+		temp_center = STransform * Projection * CTransform * temp_center;
+		f_normal = STransform * Projection * CTransform * f_normal;
+		vec3 n = vec4t3(f_normal);
+		vec3 c = vec4t3(temp_center);
+
+		if (show_normalsF) {
+			Drawline(c.x, n.x, c.y, n.y, 't');
+		}
+		
+	}
+
+	for (int i = length; i < vertices->size(); i += 3)
+	{
+		GLfloat x[3] = { 0 };
+		GLfloat y[3] = { 0 };
+
+		for (int j = 0; j < 3; j++) {
+			vec4 temp = WTransform * MTransform * vec4((*vertices)[i + j]);
+			temp = STransform * Projection * CTransform * temp;
+			vec3 v = vec4t3(temp);
+			x[j] = v.x;
+			y[j] = v.y;
+
+		}
+
+		char color = 'r';
+
+		Drawline(x[0], x[1], y[0], y[1], color);
+		Drawline(x[2], x[1], y[2], y[1], color);
+		Drawline(x[0], x[2], y[0], y[2], color);
+	}
+
+}
+
+void Renderer::ClearColorBuffer() {
+	//clean bufer
+	for (int i = 0; i < m_width; i++) {
+		for (int j = 0; j < m_height; j++) {
+			m_outBuffer[INDEX(m_width, i, j, 0)] = 0;
+			m_outBuffer[INDEX(m_width, i, j, 1)] = 0;
+			m_outBuffer[INDEX(m_width, i, j, 2)] = 0;
+		}
+	}
+}
+
+void Renderer::reshape(int width, int height) {
+	CreateBuffers(width, height);
+}
+
+void Renderer::CreateLocalBuffer() {
+}
 
 /////////////////////////////////////////////////////
 //OpenGL stuff. Don't touch.
@@ -126,4 +330,40 @@ void Renderer::SwapBuffers()
 	a = glGetError();
 	glutSwapBuffers();
 	a = glGetError();
+}
+
+void Renderer::ClearDepthBuffer() {
+	//clean bufer
+	for (int i = 0; i < m_width; i++)
+		for (int j = 0; j < m_height; j++) {
+			m_zbuffer[INDEX(m_width, i, j, 0)] = 0;
+		}
+}
+
+void Renderer::SetCameraMatrices(const mat4& cTransform, const mat4& projection) {
+	CTransform = cTransform;
+	Projection = projection;
+}
+void Renderer::SetScreenTransform(GLfloat min_x, GLfloat min_y, GLfloat max_x, GLfloat max_y) {
+	STransform[0][3] = m_width / 2;
+	STransform[1][3] = m_height / 2;
+	STransform[0][0] = m_width / 2;
+	STransform[1][1] = m_height / 2;
+}
+void Renderer::SetObjectMatrices(const mat4& mTranslate, const mat4& mTransform, const mat4& wTransform, const mat4& nTransform) {
+	MTransform = mTransform;
+	NTransform = nTransform;
+	WTransform = wTransform;
+	MTranslate = mTranslate;
+}
+
+void Renderer::SetFlags(bool bbox, bool show_normalsV, bool show_normalsF) {
+	this->bbox = bbox;
+	this->show_normalsF = show_normalsF;
+	this->show_normalsV = show_normalsV;
+}
+
+void Renderer::Init() {
+	ClearColorBuffer();
+	ClearDepthBuffer();
 }
--- CG/CG_skel_w_MFC/Scene.h
+++ CG/CG_skel_w_MFC/Scene.h
@@ -18,38 +18,76 @@ class Light {
 };
 
 class Camera {
+	
+public:
 	mat4 cTransform;
 	mat4 projection;
+	mat4 S;
+	vec3 eye;
+	vec3 at;
+	vec3 up;
+	bool rendered = false;
 
-public:
+	Camera();
+	Camera(vec3 eye, vec3 at, vec3 up);
+	~Camera() {}
 	void setTransformation(const mat4& transform);
-	void LookAt(const vec4& eye, const vec4& at, const vec4& up );
-	void Ortho( const float left, const float right,
+	void LookAt(const vec4& eye, const vec4& at, const vec4& up);
+	void Ortho(const float left, const float right,
 		const float bottom, const float top,
-		const float zNear, const float zFar );
-	void Frustum( const float left, const float right,
-		const float bottom, const float top,
-		const float zNear, const float zFar );
-	mat4 Perspective( const float fovy, const float aspect,
 		const float zNear, const float zFar);
-
+	void Frustum(const float left, const float right,
+		const float bottom, const float top,
+		const float zNear, const float zFar);
+	void Perspective(const float fovy, const float aspect,
+		const float zNear, const float zFar);
 };
 
 class Scene {
 
-	vector<Model*> models;
+	
 	vector<Light*> lights;
-	vector<Camera*> cameras;
+	
 	Renderer *m_renderer;
 
+	void _add_line(Model* model, vec3 v1, vec3 v2, vec3 v3);
+
 public:
-	Scene() {};
-	Scene(Renderer *renderer) : m_renderer(renderer) {};
-	void loadOBJModel(string fileName);
-	void draw();
-	void drawDemo();
-	
+	vector<Model*> models;
+	vector<Camera*> cameras;
 	int activeModel;
 	int activeLight;
 	int activeCamera;
+	GLfloat step_move;
+	GLfloat step_scale;
+	GLfloat step_rotate;
+
+	
+	Scene();
+	Scene(Renderer* renderer);
+	~Scene();
+	void loadOBJModel(string fileName);
+	void perspective(const float fovy, const float aspect, const float zNear, const float zFar);
+	void frustum(const float left, const float right, const float bottom, const float top, const float zNear, const float zFar);
+	void ortho(const float left, const float right, const float bottom, const float top, const float zNear, const float zFar);
+
+	void draw();
+	void drawDemo();
+	
+	void showNormalsV();
+	void showNormalsF();
+	void removeNormalsV();
+	void removeNormalsF();
+	void addPrim();
+	void addCam(string s, vec3 eye, vec3 at, vec3 up);
+	void render();
+	void unrender();
+	void rotate(char cord);
+	void zoomIn();
+	void zoomOut();
+	void bbox();
+	void unbbox();
+	void focus();
+	void scale(char dir);
+	void move(int dx, int dy);
 };
